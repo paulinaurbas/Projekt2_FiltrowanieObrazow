5cf72e97bf29307190f1b8344fa4c8e73dae0586
paulinaurbas <urbas.paulina@gmail.com>
2018-06-23 20:55:44 +0200

    Gotowy do oddania

--- appli/User.c
+++ appli/User.c
@@ -11,7 +11,7 @@ void ErrorInfo()
 }
 int TerminalCheck(int argc, char * argv[], Image * readyImage)
 {
-	if (argc >= 5 && strcmp(argv[1], "-i") == 0 && strcmp(argv[3], "-o") == 0)
+	if (argc > 5 && strcmp(argv[1], "-i") == 0 && strcmp(argv[3], "-o") == 0)
 	{
 		return 0;
 }
@@ -36,9 +36,7 @@ int Filtr(char * argv[], Image * readyImage)
 	}
 	if (strcmp(argv[5], "-3") == 0)
 	{
-		printf_s("Wybierz poziom Inversji: ");
-		scanf_s("%d", &changenumber);
-		Inversion(readyImage, changenumber);
+		Inversion(readyImage);
 		return 0;
 	}
 	if (strcmp(argv[5], "-4") == 0)
@@ -76,10 +74,12 @@ void Help()
 	printf_s("Aby podac nazwe pliku wejsciowego nalezy uzyc komendy -i, a nastepnie podac nazwe pliku wejsciowego \n");
 	printf_s("Aby podac nazwe pliku wyjsciowego nalezy uzyc komendy -o, a nastepnie podac nazwe pliku wyjsciowego \n");
 	printf_s("Aby podac filtru nalezy podac:\n");
-	printf_s("1 - filtr dolnoprzepustowy \n");
-	printf_s("2 - filtr gornoprzepustowy \n");
-	printf_s("3 - filtr medianowy \n");
-	printf_s("4 - filtr usredniajacy \n");
-	printf_s("5 - filtr minimalny \n");
-	printf_s("6 - filtr maksymalny \n");
+	printf_s("-1 - czarnobialy \n");
+	printf_s("-2 - sepia \n");
+	printf_s("-3 - negatyw \n");
+	printf_s("-4 - jasnosc \n");
+	printf_s("-5 - kontrast \n");
+	printf_s("-6 - wygladzenie \n");
+	printf_s("-7 - krawedziowanie \n");
+	printf_s("Przykladowe wywolanie -i SwietoHoli.bmp -o Wyjscie -1 \n");
 }
--- appli/User.h
+++ appli/User.h
@@ -7,8 +7,28 @@
 #include "User.h"
 #include "Function.h"
 #include "BMPRead.h"
+/** Function printing help
+@author Paulina Urbas
+@date 20.05.2018
+*/
 void Help();
+/** Function checking terminal
+@author Paulina Urbas
+@date 20.05.2018
+@param int, char *, Image *
+@return int
+*/
 int TerminalCheck(int argc, char * argv[], Image * readyImage);
+/** Function checking number of filtr and coverting
+@author Paulina Urbas
+@date 2.05.2018
+@param char *, Image * 
+@return int
+*/
 int Filtr(char * argv[], Image * readyImage);
+/** Function printing errorinfo
+@author Paulina Urbas
+@date 20.05.2018
+*/
 void ErrorInfo();
 #endif
